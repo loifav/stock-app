360e4d9793c0b289234bec8fe981a4a4da5609a4
loifav <favrelc22@gmail.com>
2025-10-20 14:00:41 +0200

    try new file with .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es2021: true },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["react"],
+  settings: { react: { version: "detect" } },
+  rules: {
+    semi: ["error", "always"],
+    "prefer-const": "error",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    eqeqeq: "error",
+    "react/react-in-jsx-scope": "off",
+  },
+};
